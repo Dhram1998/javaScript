e1aeecd148858746bc620cab0161da7ef7902b9f
Dharmendra Jha <115442520+Dhram1998@users.noreply.github.com>
2024-01-23 13:18:08 +0000

    container config done

---
+++ Basic/variable.js
@@ -0,0 +1,11 @@
+const accountId = 1234
+let accountEmail = "rahuljha@gmail.com"
+var accountPassword = "1419"
+accountCity = "Delhi"
+let accountState;
+
+// accountId = 2 , not allowed
+
+// console.log(accountId)
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
