5f81f6582fde52a33f0551e0324eb5b22851e28f
Dharmendra Jha <115442520+Dhram1998@users.noreply.github.com>
2024-01-24 11:11:28 +0000

    conversion of data type

---
+++ Basic/conversionOperation.js
@@ -0,0 +1,38 @@
+// let score = 33
+
+// console.log(typeof (score));
+
+// let num = "33abc"
+
+// console.log(typeof num);
+
+// let valueInNumber = Number(num)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// let n = null
+
+// console.log(typeof n);
+
+// let valueNumber = Number(n)
+// console.log(typeof valueNumber);
+// console.log(valueNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+// "" => false
+// "Rahul" => true
+
+// let isLoggedIn = 0
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
--- Basic/variable.js
+++ Basic/variable.js
@@ -9,3 +9,24 @@ let accountState;
 // console.log(accountId)
 
 console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) we are using nodejs, not browser
+
+/********************Data Type*************/
+// number
+// bigint
+// string
+// boolean
+// null => standalone value
+// undefined => 
+// symbol => unique
+// boject
+
+
+
+console.log(typeof "Rahul");
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
